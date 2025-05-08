9f1660401370f05266ef772df23fbf05e3792aa7
stive1213 <estifanosamsalu833@gmail.com>
2025-05-08 12:04:28 -0700

    aded index file

---
+++ backend/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const port = 3000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Basic route to test the backend
+app.get('/', (req, res) => {
+  res.json({ message: 'Backend is working!' });
+});
+
+// Start Express server
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
